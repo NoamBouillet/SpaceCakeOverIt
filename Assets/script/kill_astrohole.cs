190f0d8e115b4626ed7338386adf72a6bc212e19
leandreEpitech <leandre.fouret@epitech.eu>
2024-12-15 18:55:02 +0100

    feat: added black hole aspiration animation

--- Assets/script/kill_astrohole.cs
+++ Assets/script/kill_astrohole.cs
@@ -1,13 +1,36 @@
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
 
-public class kill_astrohole : MonoBehaviour
+public class KillAstrohole : MonoBehaviour
 {
     public Rigidbody2D rb;
+    public Transform player_transform;
+
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player")) {
-            rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        if (collision.CompareTag("Player"))
+        {
+            // Start the scaling coroutine
+            StartCoroutine(ScaleDownCoroutine());
         }
+    }
+
+    private IEnumerator ScaleDownCoroutine()
+    {
+        float duration = 0.1f;
+        Vector3 originalScale = player_transform.localScale;
+        float elapsedTime = 0f;
+
+        rb.constraints = RigidbodyConstraints2D.FreezePosition;
+        while (elapsedTime < duration)
+        {
+            player_transform.localScale = Vector3.Lerp(originalScale, Vector3.zero, elapsedTime / duration);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        player_transform.localScale = Vector3.zero;
     }
 }
