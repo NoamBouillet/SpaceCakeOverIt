0bc9a1d80192ca1d661e0ecc7071f8f41f8bbb11
Laugane-epitech <logan.capoen@epitech.eu>
2024-12-15 18:48:56 +0100

    [+] score in the game over scene

--- Assets/script/death_zone.cs
+++ Assets/script/death_zone.cs
@@ -5,7 +5,6 @@ public class death_zone : MonoBehaviour
 {
     public camera_moove camera_mooves;
     public Transform camera_transform;
-    public Transform player;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
